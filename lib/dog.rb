39e35bba109e92141365dea16661b8cbc2c51dac
EveOfMaly <john.a.kyeremeh@gmail.com>
2021-12-30 18:58:20 -0500

    commit tree 78e5168eeac545439807893474358f5cb8fb4e15

--- lib/dog.rb
+++ lib/dog.rb
@@ -19,7 +19,6 @@ class Dog
     end
 
     def save 
-
         if self.id
             self.update 
         else
@@ -40,7 +39,7 @@ class Dog
     end
 
     def self.find_by_id(id_num)
-        DB[:conn].execute("SELECT * from dogs where id = ? ", id_num).map do |row|
+        DB[:conn].execute("SELECT * from dogs where id = ? Limit 1  ", id_num).map do |row|
             self.new_from_db(row)
         end.first
     end
@@ -53,7 +52,6 @@ class Dog
         if !dog.empty?
             dog_data = dog[0]
             dog = self.new_from_db(dog_data)
-            # dog = Dog.new(id: dog_data[0],name: dog_data[1], breed: dog_data[2])
         #if dog exist point to the existing dog instance,
         else
             dog = self.create(attr)
@@ -63,7 +61,7 @@ class Dog
 
     def self.find_by_name(name)
 
-        DB[:conn].execute("SELECT * from dogs where name = ? ", name).map do |row|
+        DB[:conn].execute("SELECT * from dogs where name = ? Limit 1 ", name).map do |row|
             self.new_from_db(row)
         end.first
 
@@ -85,83 +83,3 @@ end
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# attr_accessor :name, :breed
-    # attr_reader :id
-
-    # def initialize(attribute)
-    #     @id = attribute[:id]
-    #     @name = attribute[:name]
-    #     @breed = attribute[:breed]
-    # end
-
-    # def self.create_table
-    #     DB[:conn].execute( "CREATE TABLE IF NOT EXISTS dogs (id INTEGER PRIMARY KEY, name TEXT, breed TEXT)")
-    # end
-
-    # def self.drop_table
-    #     DB[:conn].execute( "DROP TABLE IF EXISTS dogs")
-    # end
-
-    # def save
-    #     if self.id
-    #         self.update
-    #     else
-    #         DB[:conn].execute("INSERT INTO dogs (name, breed) VALUES (?, ?)", self.name, self.breed)
-    #         @id =  DB[:conn].execute("SELECT last_insert_rowid() from dogs")[0][0]
-    #     end
-    #     self
-    # end
-
-    # def self.create(attributes)
-    #     new_dog = Dog.new(attributes)
-    #     new_dog.save 
-    #     new_dog
-    # end
-
-    # def self.new_from_db(row)
-    #     dog_id = row[0]
-    #     dog_name = row[1]
-    #     dog_breed = row[2]
-
-    #     attributes = {id: dog_id, name: dog_name, breed: dog_breed}
-
-    #     self.new(attributes)
-    # end
-
-    # def self.find_by_name(name_in_database)
-    #     row = DB[:conn].execute( "SELECT * FROM dogs WHERE name = ? LIMIT 1", name_in_database)
-    #     self.new_from_db(row)
-    #     self.name
-    # end
-
-    # def self.find_by_id(id_num)
-    #     dog_info = DB[:conn].execute( "SELECT * FROM dogs WHERE id = ? LIMIT 1", id_num).flatten
-    #     self.new_from_db(db_info)
-    #     self.id
-    # end
-
-
-    # def update
-    #     sql = "UPDATE dogs SET name = ?, breed = ? where id = ?"
-    #     DB[:conn].execute(sql, self.name, self.breed, self.id)
-    # end
